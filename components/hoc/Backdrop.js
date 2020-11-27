eacef9b0f5cd571d9409b029b8dbecdf9b8cdde2
asabogal <asabogal.git@gmail.com>
2020-11-27 16:08:37 -0500

    migrate Layout and Backdrop HOC

---
+++ components/hoc/Backdrop.js
@@ -0,0 +1,17 @@
+import styled from 'styled-components';
+
+export const Backdrop = (props) => (
+  <BackdropContainer onClick={props.click}></BackdropContainer>
+);
+
+ const BackdropContainer = styled.div`
+  position: fixed;
+  width: 100%;
+  height: 100%;
+  background: rgba(0,0,0,0.5);
+  top: 0;
+  left: 0;
+  z-index: 901;
+`;
+
+export default Backdrop;
---
+++ components/hoc/Layout.js
@@ -0,0 +1,49 @@
+import {useState} from 'react';
+import styled from 'styled-components';
+import NavHeader from '../navigation/NavHeader';
+import SideNav from '../navigation/SideNav';
+import Backdrop from './Backdrop';
+import Footer from '../navigation/Footer';
+import MobileFooter from '../navigation/MobileFooter';
+
+const Layout = (props) => {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const toggleSideNav = () => {
+    setIsOpen(!isOpen);
+  };
+
+  const closeSideNav = () => {
+    setIsOpen(false);
+  };
+
+  let backdrop;
+  if (isOpen) {
+    backdrop = <Backdrop click={closeSideNav}/>
+  };
+
+  return (
+    <LayoutContainer>
+      <NavHeader toggleSideNav={toggleSideNav}/>
+      <SideNav open={isOpen} click={closeSideNav}/>
+      {backdrop}
+      {props.children}
+      <Footer/>
+      <MobileFooter/>
+    </LayoutContainer>
+  );
+};
+
+export default Layout;
+
+const LayoutContainer = styled.div`
+  position: relative;
+  nav {
+    display: none;
+  }
+  @media (max-width: 500px) {
+    nav {
+      display: block;
+    }
+  }
+`;
