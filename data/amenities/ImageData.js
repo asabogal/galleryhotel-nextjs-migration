ec16445956c938d9f08f8d393f27c88bab014653
asabogal <asabogal.git@gmail.com>
2020-11-28 10:17:24 -0500

    fix image paths

--- data/amenities/ImageData.js
+++ data/amenities/ImageData.js
@@ -1,33 +1,33 @@
 // gallery
-import galleryWide from '../../images/amenities/gallery/gallery_wide.png';
-import gallery2Long from '../../images/amenities/gallery/gallery2_long.png'
-import gallery3Wide from '../../images/amenities/gallery/gallery3_wide.png';
-import gallery4Wide from '../../images/amenities/gallery/lamp_wide.png';
-import gallery5Long from '../../images/amenities/gallery/lounge_long.png';
+import galleryWide from '../../public/images/amenities/gallery/gallery_wide.png';
+import gallery2Long from '../../public/images/amenities/gallery/gallery2_long.png'
+import gallery3Wide from '../../public/images/amenities/gallery/gallery3_wide.png';
+import gallery4Wide from '../../public/images/amenities/gallery/lamp_wide.png';
+import gallery5Long from '../../public/images/amenities/gallery/lounge_long.png';
 // garden
-import gardenWide from '../../images/amenities/garden/garden_wide.png';
-import garden2Wide from '../../images/amenities/garden/garden2_wide.png';
-import garden3Wide from '../../images/amenities/garden/garden3_wide.png';
-import garden4Wide from '../../images/amenities/garden/garden4_wide.png';
-import garden5Wide from '../../images/amenities/garden/garden5_wide.png';
-import garden6Wide from '../../images/amenities/garden/garden6_wide.png';
+import gardenWide from '../../public/images/amenities/garden/garden_wide.png';
+import garden2Wide from '../../public/images/amenities/garden/garden2_wide.png';
+import garden3Wide from '../../public/images/amenities/garden/garden3_wide.png';
+import garden4Wide from '../../public/images/amenities/garden/garden4_wide.png';
+import garden5Wide from '../../public/images/amenities/garden/garden5_wide.png';
+import garden6Wide from '../../public/images/amenities/garden/garden6_wide.png';
 // lobby
-import lobbyWide from '../../images/amenities/lobby/lobby_wide.png';
-import lobby2Long from '../../images/amenities/lobby/lobby2_long.png';
-import lobby3Wide from '../../images/amenities/lobby/lobby3_wide.png';
-import lobby4Long from '../../images/amenities/lobby/lobby4_long.png';
-import lobby5Wide from '../../images/amenities/lobby/lobby5_wide.png';
-import lobby6Long from '../../images/amenities/lobby/lobby6_long.jpg';
+import lobbyWide from '../../public/images/amenities/lobby/lobby_wide.png';
+import lobby2Long from '../../public/images/amenities/lobby/lobby2_long.png';
+import lobby3Wide from '../../public/images/amenities/lobby/lobby3_wide.png';
+import lobby4Long from '../../public/images/amenities/lobby/lobby4_long.png';
+import lobby5Wide from '../../public/images/amenities/lobby/lobby5_wide.png';
+import lobby6Long from '../../public/images/amenities/lobby/lobby6_long.jpg';
 // mediaRoom
-import mediaWide from '../../images/amenities/media_room/media_wide.png';
-import media2Wide from '../../images/amenities/media_room/media2_wide.png';
-import media3Wide from '../../images/amenities/media_room/media3_wide.png';
-import media4Wide from '../../images/amenities/media_room/media4_wide.png';
+import mediaWide from '../../public/images/amenities/media_room/media_wide.png';
+import media2Wide from '../../public/images/amenities/media_room/media2_wide.png';
+import media3Wide from '../../public/images/amenities/media_room/media3_wide.png';
+import media4Wide from '../../public/images/amenities/media_room/media4_wide.png';
 // reception
-import receptionWide from '../../images/amenities/reception/reception_wide.png';
-import reception2Wide from '../../images/amenities/reception/reception2_wide.png';
-import reception3Wide from '../../images/amenities/reception/reception3_wide.png';
-import reception4Wide from '../../images/amenities/reception/reception4_wide.png';
+import receptionWide from '../../public/images/amenities/reception/reception_wide.png';
+import reception2Wide from '../../public/images/amenities/reception/reception2_wide.png';
+import reception3Wide from '../../public/images/amenities/reception/reception3_wide.png';
+import reception4Wide from '../../public/images/amenities/reception/reception4_wide.png';
 
 export const amenityImages = [
   {
