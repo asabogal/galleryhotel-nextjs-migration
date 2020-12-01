2ee5224beb05fd1bc9c4cbcd79379d8cac674744
asabogal <asabogal.git@gmail.com>
2020-11-30 19:41:45 -0500

    update HeadMessage and pass on Layout

--- components/hoc/Layout.js
+++ components/hoc/Layout.js
@@ -1,5 +1,6 @@
 import {useState} from 'react';
 import styled from 'styled-components';
+import HeadMessage from '../navigation/HeadMessage';
 import NavHeader from '../navigation/NavHeader';
 import SideNav from '../navigation/SideNav';
 import Backdrop from './Backdrop';
@@ -24,6 +25,7 @@ const Layout = (props) => {
 
   return (
     <LayoutContainer>
+      <HeadMessage/>
       <NavHeader toggleSideNav={toggleSideNav}/>
       <SideNav open={isOpen} click={closeSideNav}/>
       {backdrop}
--- components/navigation/HeadMessage.js
+++ components/navigation/HeadMessage.js
@@ -3,7 +3,7 @@ import styled from 'styled-components';
 const HeadMessage = () => {
   return (
     <Container>
-      <p>Rooms available off-season. Please call 631-477-4000 to book</p>
+      <p>Rooms available off-season. Please call us at <a href='tel: 631-477-4000'>631-477-4000</a> to book</p>
     </Container>
   );
 };
@@ -13,14 +13,18 @@ export default HeadMessage;
 const Container= styled.div`
   z-index: 999;
   background-color: #28486b;
-  color: lightgray;
-  font-size: 12px;
-  height: 25px;
+  font-weight: 900;
+  color: white;
+  font-size: 14px;
+  height: 30px;
   text-align:center; 
   width: 100%;
   overflow: hidden;
   p {
     margin: auto;
     padding-top: 4px;
+  }
+  a {
+    color: inherit;
   }
 `;
