0636667c601926cbbac5bad29a58f495ebe3674b
Alejandro Sabogal <asabogal.dev@gmail.com>
2021-03-11 17:13:46 -0500

    update keywords

--- pages/_app.js
+++ pages/_app.js
@@ -81,7 +81,7 @@ const App = ({ Component, pageProps }) => {
         <meta charSet="utf-8" />
         <title>The Gallery Hotel</title>
         <meta name="description" content="The Gallery Hotel - A stylish boutique hotel and contemporary art gallery in the heart Greenport, New York" />
-        <meta name="keywords" content="Gallery Hotel, Gallery, Hotel, The Gallery Hotel, Greenport, Greenport Hotel, greenport hotel, hotel, gallery, long island, greenport new york hotel" />
+        <meta name="keywords" content="Gallery Hotel, Gallery, Hotel, The Gallery Hotel, Greenport, Greenport Hotel, greenport hotel, hotel, hotels, gallery, long island, new york, hamptons, greenport new york hotel" />
         <link rel="icon" type="image/png" href="./favicon.ico" sizes="16x16" />
         <link href="https://fonts.googleapis.com/css?family=Poppins:100,200,300,400,500,600,700,800,900&display=swap" rel="stylesheet" />
       </Helmet>
